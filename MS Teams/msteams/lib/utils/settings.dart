0168c43fdf838c3c933b1cb37508680cabb6fc16
Gaurisha21 <68791455+Gaurisha21@users.noreply.github.com>
2021-07-13 23:24:02 +0530

    Final Project

--- MS Teams/msteams/lib/utils/settings.dart
+++
@@ -1,4 +0,0 @@
-/// Define App ID and Token
-const APP_ID = 'a0d6887ecee148798801e4d404672cbf';
-// String Token =
-//     '0067ba5d031b67c454b9ce01b539762da89IAAy1biwwX1yF+/JKN9SPvH+glDZKLtHBumKvo2SpkwgcZKNJKAAAAAAEAC4541oWoTtYAEAAQBahO1g';
